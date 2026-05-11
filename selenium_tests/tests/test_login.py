4ff68f97bfc08ce1a63823e16c43c2461a3ab951
kirbytheone <leimtheone@gmail.com>
2026-05-11 16:06:14 +0800

    Added pytest+selenium parametrized logging tests

--- selenium_tests/tests/test_login.py
+++ selenium_tests/tests/test_login.py
@@ -1,4 +1,24 @@
+import pytest
+
 from selenium_tests.pages.login_page import LoginPage
+
+
+@pytest.mark.parametrize(
+    "username, password, expected_message",
+    [
+        ("tomsmith", "wrong_password", "Your password is invalid!"),
+        ("wrong_user", "SuperSecretPassword!", "Your username is invalid!"),
+    ],
+)
+
+def test_invalid_login(driver, username, password, expected_message):
+    page = LoginPage(driver)
+
+    page.open_page()
+    page.login(username, password)
+
+    message = page.get_error_message()
+    assert expected_message in message
 
 def test_login_success(driver):
     page = LoginPage(driver)
@@ -9,13 +29,3 @@ def test_login_success(driver):
     message = page.get_success_message()
 
     assert 'You logged into a secure area!' in message
-
-def test_invalid_login(driver):
-    page = LoginPage(driver)
-
-    page.open_page()
-    page.login('tomsmith', 'Wrong Password!')
-
-    message = page.get_error_message()
-
-    assert 'Your password is invalid!' in message
