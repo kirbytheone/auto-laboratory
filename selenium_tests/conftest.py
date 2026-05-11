edc349959910674580dc323aa596c345664c5302
kirbytheone <leimtheone@gmail.com>
2026-05-11 15:21:48 +0800

    Automatic screenshot capture for failed tests added

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -1,17 +1,32 @@
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+SCREENSHOTS_DIR = Path(__file__).resolve().parent.parent / "screenshots"
+
 @pytest.fixture
-def driver():
+def driver(request):
     options = Options()
-
     # options.add_argument("--headless")
-
     options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(options=options)
 
     yield driver
 
+    if request.node.rep_call.failed:
+        SCREENSHOTS_DIR.mkdir(exist_ok=True)
+        test_name = request.node.name
+        driver.save_screenshot(str(SCREENSHOTS_DIR / f'{test_name}.png'))
+
     driver.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
--- selenium_tests/tests/test_checkboxes.py
+++ selenium_tests/tests/test_checkboxes.py
@@ -6,6 +6,8 @@ def test_check_first_checkbox(driver):
     page.open_page()
     page.check_checkbox(0)
 
+    assert False
+
     assert page.is_checkbox_selected(0)
 
 def test_uncheck_second_checkbox(driver):
