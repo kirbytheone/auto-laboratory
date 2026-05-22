bcc02e8caa1c4516505f64d8635699ffd9eb6fb4
kirbytheone <leimtheone@gmail.com>
2026-05-23 01:15:08 +0800

    Added Django model tests for tasks and comments, also apdated req and pytest.ini

---
+++ django_tests/test_task_model.py
@@ -0,0 +1,104 @@
+import pytest
+from django.contrib.auth.models import User
+
+from tasks.models import Task, Comment
+
+
+@pytest.mark.django_db
+def test_task_creation():
+    user = User.objects.create_user(
+        username="testuser",
+        password="testpass123",
+    )
+
+    task = Task.objects.create(
+        title="Test TASK",
+        description="Test description",
+        status=Task.Status.TODO,
+        priority=Task.Priority.MEDIUM,
+        owner=user,
+    )
+
+    assert task.title == "Test TASK"
+    assert task.description == "Test description"
+    assert task.status == Task.Status.TODO
+    assert task.priority == Task.Priority.MEDIUM
+    assert task.owner == user
+
+@pytest.mark.django_db
+def test_comment_creation_for_task():
+    user = User.objects.create_user(
+        username="commentuser",
+        password="testpassword123",
+    )
+
+    task = Task.objects.create(
+        title="Test Task with comment",
+        owner=user,
+    )
+
+    comment = Comment.objects.create(
+        task=task,
+        author=user,
+        text="This is a test comment",
+    )
+
+    assert comment.task == task
+    assert comment.author == user
+    assert comment.text == "This is a test comment"
+    assert task.comments.count() == 1
+    assert task.comments.first() == comment
+
+@pytest.mark.django_db
+def test_deleting_task_related_comments():
+    user = User.objects.create_user(
+        username="testuser",
+        password="testpass123",
+    )
+
+    task = Task.objects.create(
+        title="Task with comments",
+        owner=user,
+    )
+
+    Comment.objects.create(
+        task=task,
+        author=user,
+        text="Comment 1",
+    )
+
+    Comment.objects.create(
+        task=task,
+        author=user,
+        text="Comment 2",
+    )
+
+    assert Comment.objects.count() == 2
+
+    task.delete()
+
+    assert Comment.objects.count() == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
