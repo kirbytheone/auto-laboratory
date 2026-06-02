798bcce7d32d3d8e3ea7f4fb690d19ecf41a45a4
kirbytheone <leimtheone@gmail.com>
2026-06-02 23:56:03 +0800

    Added django test for attacments task

---
+++ django_tests/test_task_attachments.py
@@ -0,0 +1,49 @@
+import pytest
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from tasks.models import Task, Attachment
+
+
+@pytest.mark.django_db
+def test_logged_user_can_upload_attachment_to_task(client):
+    user = User.objects.create_user(
+        username="attachmentviewuser",
+        password="testpass123",
+    )
+
+    task = Task.objects.create(
+        title="Task For Attachment",
+        owner=user,
+    )
+
+    client.login(
+        username="attachmentviewuser",
+        password="testpass123",
+    )
+
+    uploaded_file = SimpleUploadedFile(
+        name="view_upload.txt",
+        content=b"Attachment from view test",
+        content_type="text/plain",
+    )
+
+    response = client.post(
+        f"/tasks/{task.id}/",
+        {
+            "file": uploaded_file,
+        },
+    )
+
+    assert response.status_code == 302
+    assert Attachment.objects.count() == 1
+
+    attachment = Attachment.objects.first()
+    print(attachment.file.name)
+
+    print(attachment.file.path)
+
+    assert attachment.task == task
+    assert attachment.uploaded_by == user
+    assert attachment.filename.startswith("view_upload")
+    assert attachment.filename.endswith(".txt")
