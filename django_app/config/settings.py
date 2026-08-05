200ce2bb7bee39995e368f468405c33a7649f33d
kirbytheone <leimtheone@gmail.com>
2026-08-05 18:09:21 +0300

    Added initial UI styling and task card layout

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -153,6 +153,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 LOGIN_URL = "/admin/login/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
