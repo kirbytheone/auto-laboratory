3bd9ee4277fb150c442a860ccac4432888e46c95
kirbytheone <leimtheone@gmail.com>
2026-08-12 18:25:56 +0300

    migrate API authentication from DRF tokens to JWT

--- django_app/tasks/api/urls.py
+++ django_app/tasks/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import (
     AttachmentListCreateAPIView,
@@ -12,7 +11,6 @@ from .views import (
 urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-    path('token/', obtain_auth_token, name='api-token'),
     path('tasks/', TaskListAPIView.as_view(), name='api-task-list'),
     path('tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='api-task-detail'),
     path('tasks/<int:task_pk>/comments/', CommentListCreateAPIView.as_view(), name='api-task-comment-list'),
--- django_tests/test_api_authentication.py
+++ django_tests/test_api_authentication.py
@@ -3,14 +3,91 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_user_can_obtain_api_token(api_client, create_user):
+def test_valid_refresh_token_returns_new_access_token(api_client, create_user):
+    create_user(
+        username='refresh_user_access_token',
+        password='securepass123',
+    )
+
+    token_response = api_client.post(
+        reverse('token_obtain_pair'),
+        {
+            'username': 'refresh_user_access_token',
+            'password': 'securepass123',
+        },
+        format='json',
+    )
+
+    refresh_token = token_response.json()['refresh']
+
+    refresh_response = api_client.post(
+        reverse('token_refresh'),
+        {'refresh': refresh_token,
+         },
+        format='json',
+    )
+
+    assert refresh_response.status_code == 200
+    assert 'access' in refresh_response.data
+    assert refresh_response.data['access']
+
+@pytest.mark.django_db
+def test_refresh_token_cannot_authenticate_task_api(api_client, create_user, create_task):
+    user = create_user(
+        username='refresh_auth_user',
+        password='securepass123',
+    )
+    create_task(
+        owner=user,
+        title='Protected Task',
+    )
+
+    token_response = api_client.post(
+        reverse('token_obtain_pair'),
+        {
+            'username': 'refresh_auth_user',
+            'password': 'securepass123',
+        },
+        format='json',
+    )
+
+    refresh_token = token_response.json()['refresh']
+
+    api_client.credentials(
+        HTTP_AUTHORIZATION=f'Bearer {refresh_token}'
+    )
+
+    response = api_client.get(reverse('api-task-list'))
+
+    assert response.status_code == 401
+
+@pytest.mark.django_db
+def test_session_authentication_does_not_allow_api_access(api_client, create_user):
+    create_user(
+        username='session_user',
+        password='securepass123',
+    )
+
+    logged_in = api_client.login(
+        username='session_user',
+        password='securepass123',
+    )
+
+    assert logged_in is True
+
+    response = api_client.get(reverse('api-task-list'))
+
+    assert response.status_code == 401
+
+@pytest.mark.django_db
+def test_user_can_obtain_jwt_token(api_client, create_user):
     create_user(
         username='token_user',
         password='securepass123',
     )
 
     response = api_client.post(
-        reverse('api-token'),
+        reverse('token_obtain_pair'),
         {
             'username': 'token_user',
             'password': 'securepass123',
@@ -19,18 +96,20 @@ def test_user_can_obtain_api_token(api_client, create_user):
     )
 
     assert response.status_code == 200
-    assert 'token' in response.data
-    assert response.data['token']
+    assert 'access' in response.data
+    assert 'refresh' in response.data
+    assert response.data['access']
+    assert response.data['refresh']
 
 @pytest.mark.django_db
-def test_token_cannot_be_obtained_with_invalid_auth(api_client, create_user):
+def test_jwt_tokens_cannot_be_obtained_with_invalid_auth(api_client, create_user):
     create_user(
         username='invalid_token_user',
         password='correctpass123',
     )
 
     response = api_client.post(
-        reverse('api-token'),
+        reverse('token_obtain_pair'),
         {
             'username': 'invalid_token_user',
             'password': 'wrongpass123',
@@ -38,11 +117,39 @@ def test_token_cannot_be_obtained_with_invalid_auth(api_client, create_user):
         format='json',
     )
 
-    assert response.status_code == 400
-    assert 'token' not in response.data
+    assert response.status_code == 401
+    assert 'access' not in response.data
+    assert 'refresh' not in response.data
+    assert 'detail' in response.data
 
 @pytest.mark.django_db
-def test_token_allows_access_to_task_api(api_client, create_user, create_task):
+def test_inactive_user_cannot_obtain_jwt_tokens(
+        api_client,
+        create_user,
+):
+    user = create_user(
+        username='inactive_user',
+        password='securepass123',
+    )
+
+    user.is_active = False
+    user.save()
+
+    response = api_client.post(
+        reverse('token_obtain_pair'),
+        {
+            'username': 'inactive_user',
+            'password': 'securepass123',
+        },
+        format='json',
+    )
+
+    assert response.status_code == 401
+    assert 'access' not in response.data
+    assert 'refresh' not in response.data
+
+@pytest.mark.django_db
+def test_access_token_allows_access_to_task_api(api_client, create_user, create_task):
     user = create_user(
         username='token_access_user',
         password='securepass123',
@@ -53,7 +160,7 @@ def test_token_allows_access_to_task_api(api_client, create_user, create_task):
     )
 
     token_response = api_client.post(
-        reverse('api-token'),
+        reverse('token_obtain_pair'),
         {
             'username': 'token_access_user',
             'password': 'securepass123',
@@ -61,9 +168,9 @@ def test_token_allows_access_to_task_api(api_client, create_user, create_task):
         format='json',
     )
 
-    token = token_response.json()['token']
+    access_token = token_response.json()['access']
 
-    api_client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
 
     response = api_client.get(
         reverse('api-task-list')
