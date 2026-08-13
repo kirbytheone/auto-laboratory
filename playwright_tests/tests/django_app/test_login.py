87fd8bb0f98ce71e5cf28c0abba51200befa5148
kirbytheone <leimtheone@gmail.com>
2026-08-13 12:50:40 +0300

    Ruff little fix

--- playwright_tests/tests/django_app/test_login.py
+++ playwright_tests/tests/django_app/test_login.py
@@ -15,9 +15,9 @@ def test_valid_regular_user_login(page):
 
     login_page.open()
     login_page.login(username, password)
-
-    task_list_page = DjangoTaskListPage(page, BASE_URL)
     # TODO: refactor after implementation of user login setup
+    # task_list_page = DjangoTaskListPage(page, BASE_URL)
+
     # expect(page).to_have_url(f'{BASE_URL}{DjangoTaskListPage.PATH}')
     # expect(task_list_page.current_user()).to_have_text(username)
     # expect(task_list_page.logout_button).to_be_visible()
